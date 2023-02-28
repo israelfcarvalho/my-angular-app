c4bb1370750cd708214b5426151a09dbaaf9aa6f
Israel <israel.carvalho@b2wdigital.com>
2023-02-28 02:01:32 -0300

    update product details router guard
    Add a urlTree on the guard to redirect when the guard block routing

--- src/app/utils/router/guards/product-details-guard.ts
+++ src/app/utils/router/guards/product-details-guard.ts
@@ -1,5 +1,5 @@
 import { inject } from "@angular/core";
-import { CanActivateFn } from "@angular/router";
+import { CanActivateFn, Router } from "@angular/router";
 import { map } from "rxjs";
 import { ProductService } from "src/app/services/product/product.service";
 
@@ -7,6 +7,7 @@ export const productDetailsGuard: CanActivateFn = function(route){
     const productIdParam = Number.parseInt(route.paramMap.get('productId') || '')
     const productId =  Number.isNaN(productIdParam)  ? -1 : productIdParam
     const productService = inject(ProductService)
+    const routerService = inject(Router)
 
-    return productService.getById(productId).pipe(map(product => !!product))
+    return productService.getById(productId).pipe(map(product => product ? true : routerService.parseUrl('/')))
 }
